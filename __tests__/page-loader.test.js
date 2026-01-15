9f9b8a67986ce550c20865fe0eb5c1c643295af2
Alla Magina <babara@flylady.su>
2026-01-15 15:10:39 +0300

    Update dependencies and improve test configuration

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -1,27 +1,27 @@
-import fs from 'fs/promises';
-import os from 'os';
-import path from 'path';
-import nock from 'nock';
-import loadPage from '../src/index.js';
+import fs from 'fs/promises'
+import os from 'os'
+import path from 'path'
+import nock from 'nock'
+import loadPage from '../src/index.js'
 
 describe('page loader', () => {
-  let tmpDir;
+  let tmpDir
 
   beforeEach(async () => {
-    tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  });
+    tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'))
+  })
 
   test('downloads page and saves it', async () => {
-    const url = 'https://example.com';
-    const html = '<html><body>hello</body></html>';
+    const url = 'https://example.com'
+    const html = '<html><body>hello</body></html>'
 
     nock('https://example.com')
       .get('/')
-      .reply(200, html);
+      .reply(200, html)
 
-    const filePath = await loadPage(url, tmpDir);
-    const content = await fs.readFile(filePath, 'utf-8');
+    const filePath = await loadPage(url, tmpDir)
+    const content = await fs.readFile(filePath, 'utf-8')
 
-    expect(content).toBe(html);
-  });
-});
+    expect(content).toBe(html)
+  })
+})
