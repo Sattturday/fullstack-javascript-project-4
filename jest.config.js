401fdbe61e79607149a9c0f537b7fdf27392c55e
Alla Magina <babara@flylady.su>
2026-01-19 12:32:16 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,6 @@
 /** @type {import('jest').Config} */
 export default {
   testEnvironment: 'node',
-  // Тестовые файлы
-  testMatch: ['**/__tests__/**/*.test.js'],
-  // Генерация покрытия кода
-  collectCoverage: false,
-  coverageDirectory: 'coverage', // папка для отчётов
-  coverageReporters: ['json', 'lcov', 'text', 'clover'],
-
-  // Какие файлы включать в покрытие
-  collectCoverageFrom: ['src/**/*.js'],
   transform: {},
-}
+  testMatch: ['**/__tests__/**/*.test.js'],
+};
